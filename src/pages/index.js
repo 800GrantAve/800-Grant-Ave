69dda969ddf247a9261b4386a125f3c402f9381d
Alison Lee <alison2686@gmail.com>
2022-10-31 14:02:01 -0700

    remove extra slide components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import Navbar from '../components/NavBar'
 import HeroSection from '../components/HeroSection'
-// import ImageGallery from '../components/Gallery'
 import Footer from '../components/Footer'
-// import StoreImages from '../components/StoreGallery/StoreImages'
-// import StoreGallery from '../components/StoreGallery/StoreGallery'
 import SlideCarousel from '../components/SlideCarousel/SlideCarousel'
-// import SlideGallery from '../components/Slides/SlideGallery'
 import { BrowserRouter as Router } from 'react-router-dom'
 import Sidebar from '../components/Sidebar/Sidebar'
 
@@ -16,11 +12,7 @@ const Home = () => {
         <Navbar />
         <Sidebar />
         <HeroSection />
-        {/* <ImageGallery /> */}
-        {/* <StoreImages /> */}
-        {/* <StoreGallery /> */}
         <SlideCarousel />
-        {/* <SlideGallery /> */}
         <Footer />
     </Router>
   )
