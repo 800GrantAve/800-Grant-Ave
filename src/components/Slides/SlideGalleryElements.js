910b67af5d938e86182e1e656ede32782e6f9762
Alison Lee <alison2686@gmail.com>
2022-11-02 20:28:31 -0700

    change topline color to #800000

--- src/components/Slides/SlideGalleryElements.js
+++ src/components/Slides/SlideGalleryElements.js
@@ -69,7 +69,7 @@ export const TextWrapper = styled.div`
 `
 
 export const TopLine = styled.p`
-    color: #01bf71;
+    color: #800000;
     font-size: 16px;
     line-height: 16px;
     font-weight: 700;
