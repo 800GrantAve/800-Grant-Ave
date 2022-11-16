6d0954053d9ea3732f29a9c4a3e63d754aa60310
Alison Lee <alison2686@gmail.com>
2022-11-15 18:02:09 -0800

    change offset to 20 Sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -24,21 +24,21 @@ const Sidebar = ({ isOpen, toggle }) => {
                 <SidebarLink 
                     to='about' 
                     onClick={toggle}
-                    offset={40}
+                    offset={20}
                 >
                     About
                 </SidebarLink>
                 <SidebarLink 
                     to='photo' 
                     onClick={toggle}
-                    offset={40}
+                    offset={20}
                 >
                     Photo Gallery
                 </SidebarLink>
                 <SidebarLink 
                     to='footer' 
                     onClick={toggle}
-                    offset={40}
+                    offset={20}
                 >
                     Contact Us
                 </SidebarLink>
