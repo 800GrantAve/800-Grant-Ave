5f23f5451356d55166cf2d14eaed6189736adac1
Alison Lee <alison2686@gmail.com>
2022-11-15 17:31:46 -0800

    update data descriptions, fix typos

--- src/components/Slides/data.js
+++ src/components/Slides/data.js
@@ -7,7 +7,7 @@ export const SlideData =
     lightTextDesc: true,
     topLine: 'Shing Chong & Co.',
     headline: 'The History of 800 Grant Ave.',
-    description: 'According to records, Louie Wee Lee came to America about 1900. He listed himself as the son of a merchant for a store named Boew Kee & Co. He returned to China, married and had a son, then returned alone in 1906',
+    description: 'Shing Chong & Co. Store',
     img: require('../../images/store/sign1.jpg'),
     alt: 'Shing Chong & Co. Sign',
     dark: true,
@@ -105,7 +105,7 @@ export const SlideData =
         lightTextDesc: true,
         topLine: 'Shing Chong Store',
         // headline: '800 Dupont St.',
-        description: 'When he returned the store site at 800 Dupont (Grant) Ave looked like this.',
+        description: 'When he returned to San Francisco the store site at 800 Dupont (Grant) Ave looked like this.',
 
         img: require('../../images/store/clay_grant.jpg'),
         alt: 'SS Mongolia',
@@ -137,7 +137,7 @@ export const SlideData =
         lightTextDesc: true,
         topLine: 'Shing Chong Store',
         // headline: '800 Dupont St.',
-        description: '"Louie was an industrious and frugal man, stern and foreceful, but under that outer skin was a man who was respected and greeted as a good merchandiser with an innovative mind...Orders came from far and near as his promt respinse and fair dealing to friend and stranger alike became known" - Thomas Chinn',
+        description: '"Louie was an industrious and frugal man, stern and forceful, but under that outer skin was a man who was respected and greeted as a good merchandiser with an innovative mind...Orders came from far and near as his prompt response and fair dealing to friend and stranger alike became known" - Thomas Chinn',
 
         img: require('../../images/store/LouieFrugal.jpg'),
         alt: 'SS Mongolia',
@@ -153,7 +153,7 @@ export const SlideData =
         lightTextDesc: true,
         topLine: 'Shing Chong Store',
         // headline: '800 Dupont St.',
-        description: '"He was one of the dirst to experiment with Chinese delicatessen food. While such staples as roast duck and chicken, roast pig and cha siu were popular as early as the 1850\'s. Louie had his help start making his own Chinese sausage (lop cheung) and "bok shuey jee gek: (loosely translated as "white water pig\'s feet" - Thomas Chinn',
+        description: '"He was one of the first to experiment with Chinese delicatessen food. While such staples as roast duck and chicken, roast pig and cha siu were popular as early as the 1850\'s. Louie had his help start making his own Chinese sausage (lop cheung) and "bok shuey jee gek" (loosely translated as "white water pig\'s feet)" - Thomas Chinn',
 
         img: require('../../images/store/food.JPG'),
         alt: 'SS Mongolia',
