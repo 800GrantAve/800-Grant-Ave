d43cd53e7577a64b0a97c1b92036f32ed901b98f
Alison Lee <alison2686@gmail.com>
2022-11-21 14:40:15 -0800

    add swipe event

--- src/components/SlideCarousel/SlideCarousel.js
+++ src/components/SlideCarousel/SlideCarousel.js
@@ -8,13 +8,39 @@ import {
     ArrowLeft,
     ArrowRight
 } from './SlideCarouselElements'
-import { Splide, SplideSlide } from '@splidejs/react-splide'
-import '@splidejs/react-splide/css';
+
 
 const SlideCarousel = () => {
 
     const [currentSlide, setCurrentSlide] = useState(0)
+    const [touchPosition, setTouchPosition] = useState(null)
     const length = SlideData.length
+
+    const handleTouchStart = (e) => {
+        const touchDown = e.touches[0].clientX
+        setTouchPosition(touchDown)
+    }
+
+    const handleTouchMove = (e) => {
+        const touchDown = touchPosition
+
+        if(touchDown === null) {
+            return
+        }
+    
+        const currentTouch = e.touches[0].clientX
+        const diff = touchDown - currentTouch
+    
+        if (diff > 5) {
+            nextSlide()
+        }
+    
+        if (diff < -5) {
+            prevSlide()
+        }
+    
+        setTouchPosition(null)
+    }
 
     const nextSlide = () => {
         setCurrentSlide(currentSlide === length - 1 ? 0 : currentSlide + 1)
@@ -26,7 +52,10 @@ const SlideCarousel = () => {
 
   return (
     <SlideCarouselContainer id='photo'>
-            <SlideCarouselWrapper>
+            <SlideCarouselWrapper
+                onTouchStart={handleTouchStart}
+                onTouchMove={handleTouchMove}
+            >
             <ArrowLeft onClick={prevSlide} />
             <ArrowRight onClick={nextSlide} />
                     { SlideData.map((slide, index) => {
