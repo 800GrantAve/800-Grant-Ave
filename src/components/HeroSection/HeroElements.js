240e6764820c909782a1e6c319ad549ddd06ea33
Alison Lee <alison2686@gmail.com>
2023-01-06 14:05:04 -0800

    update Hero font

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -1,17 +1,16 @@
-import styled from "styled-components"
-
+import styled from "styled-components";
 
 export const HeroContainer = styled.div`
-    background: #0C0C0C;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 0 30px;
-    height: 800px;
-    position: relative;
-    z-index: 1;
+  background: #0c0c0c;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 30px;
+  height: 800px;
+  position: relative;
+  z-index: 1;
 
-    :before {
+  /* :before {
         content: '';
         position: absolute;
         top: 0;
@@ -20,88 +19,98 @@ export const HeroContainer = styled.div`
         bottom: 0;
         background: linear-gradient(180deg, rgba(0,0,0,0.2) 0%, rgba(0,0,0,0.6) 100%), linear-gradient(180deg, rgba(0,0,0,0.2) 0%, transparent 100%);
         z-index: 2;
-    }
+    } */
 
-    @media screen and (max-width: 768px) {
-        height: 720px;
-    }
+  @media screen and (max-width: 768px) {
+    height: 720px;
+  }
 `;
 
 export const HeroBg = styled.div`
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
-    max-width: 100%;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  max-width: 100%;
 `;
 
 export const ImgBg = styled.img`
-    width: 100%;
-    height: 100%;
-    
-    @media screen and (max-width: 768px) {
-        object-fit: scale-down;
-    }
+  width: 100%;
+  height: 100%;
+
+  @media screen and (max-width: 768px) {
+    object-fit: scale-down;
+  }
 `;
 
 export const HeroContent = styled.div`
-    border-radius: 15px;
-    background: #000;
-    opacity: 0.6;
-    z-index: 3;
-    /* max-width: 1200px; */
-    position: absolute;
-    padding: 0px 24px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  /* border-radius: 15px; */
+  /* background: #000; */
+  /* opacity: 0.8; */
+  z-index: 3;
+  /* max-width: 1200px; */
+  position: relative;
+  /* padding: 0px 24px; */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: left;
+  /* border: 3px solid red; */
+  right: 130px;
+  top: 322px;
+  text-transform: uppercase;
 
-    @media screen and (max-width: 768px) {
-        bottom: 5%;
-    }
+  @media screen and (max-width: 768px) {
+    /* bottom: 15%; */
+    right: 0px;
+    top: 240px;
+  }
 
-    @media screen and (max-width: 480px) {
-        bottom: 10%;
-    }
+  @media screen and (max-width: 480px) {
+    /* bottom: 15%; */
+  }
 `;
 
 export const HeroH1 = styled.h1`
-    color: #fff;
-    font-size: 42px;
-    text-align: center;
+  color: #638078;
+  font-size: 38px;
+  text-align: center;
+  font-family: "Acme", sans-serif;
+  /* font-family: "Cabin", sans-serif; */
+  /* font-family: "Paytone One", sans-serif; */
+  /* font-family: "Rammetto One", cursive; */
 
-    @media screen and (max-width: 768px) {
-        font-size: 20px;
-    }
+  @media screen and (max-width: 768px) {
+    font-size: 28px;
+  }
 `;
 
 export const HeroH2 = styled.h2`
-    color: #fff;
-    font-size: 32px;
-    text-align: center;
+  color: #fff;
+  font-size: 32px;
+  text-align: center;
 
-    @media screen and (max-width: 768px) {
-        font-size: 20px;
-    }
-
+  @media screen and (max-width: 768px) {
+    font-size: 20px;
+  }
 `;
 
 export const HeroP = styled.p`
-    margin-top: 24px;
-    color: #fff;
+  margin-top: 24px;
+  color: #fff;
+  font-size: 24px;
+  text-align: center;
+  max-width: 600px;
+
+  @media screen and (max-width: 768px) {
     font-size: 24px;
-    text-align: center;
-    max-width: 600px;
+  }
 
-    @media screen and (max-width: 768px) {
-        font-size: 24px;
-    }
-
-    @media screen and (max-width: 480px) {
-        font-size: 18px;
-    }  
+  @media screen and (max-width: 480px) {
+    font-size: 18px;
+  }
 `;
--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import { 
-    HeroContainer, 
-    HeroBg, 
-    ImgBg, 
-    HeroContent, 
-    HeroH1,
-    HeroH2
-} from './HeroElements'
-import Image from '../../images/clay_and_grant3.jpg'
-
+import React from "react";
+import {
+  HeroContainer,
+  HeroBg,
+  ImgBg,
+  HeroContent,
+  HeroH1,
+} from "./HeroElements";
+import Image from "../../images/jade-fon-chinatown.jpg";
 
 const HeroSection = () => {
-
   return (
-    <HeroContainer id='home'>
-        <HeroBg>
-            <ImgBg src={Image} />
-        </HeroBg>
-        <HeroContent>
-            <HeroH1>Learn About < br/> 800 Grant Avenue, <br /> San Francisco</HeroH1>
-            <HeroH2>The History of <br /> Shing Chong & Co. <br /> and the corner of <br /> Grant Ave & Clay St</HeroH2>
-        </HeroContent>
+    <HeroContainer id="home">
+      <HeroBg>
+        <ImgBg src={Image} />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>The History of 800 Grant Ave San Francisco</HeroH1>
+        {/* <HeroH2>
+          The History of <br /> Shing Chong & Co. <br /> and the corner of{" "}
+          <br /> Grant Ave & Clay St
+        </HeroH2> */}
+      </HeroContent>
     </HeroContainer>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
